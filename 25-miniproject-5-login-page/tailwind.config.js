f1a88d78a34633bb3a2e419b5581d480a5aef562
Abhishek Kumar <abhishek117293@gmail.com>
2024-04-28 23:07:13 +0530

    mini project 5- login page

---
+++ 25-miniproject-5-login-page/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+          fontFamily: {
+          sans: ['Rubik', 'sans-serif'],
+          mono: ['Space Mono', 'monospace'],
+        }
+    },
+  },
+  plugins: [],
+}
+

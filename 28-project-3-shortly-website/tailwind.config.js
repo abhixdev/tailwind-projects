a3c1716bbb0895546b5c5efa63f9ea80fd96edd4
Abhishek Kumar <abhishek117293@gmail.com>
2024-04-30 23:31:17 +0530

    2 Projects added

---
+++ 28-project-3-shortly-website/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      serif: ["Merriweather", "ui-serif"],
+    },
+    extend: {
+      spacing: {
+        180: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
